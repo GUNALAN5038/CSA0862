45a3ddeb0d4d3b1de21f8623b1133409c30e3e17
GUNALAN5038 <155719450+GUNALAN5038@users.noreply.github.com>
2024-03-01 10:44:03 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,3 @@
+l=eval(input("enter the length: "))
+b=eval(input("enter the breadth: "))
+print("area of rectangle: ",l*b)
---
+++ area of triangle.py
@@ -0,0 +1,3 @@
+b=eval(input("enter breadth: "))
+h=eval(input("enter height: "))
+print("area of triangle: ",1/2*b*h)
